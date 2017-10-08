6c682e130e24a22fcf6388105360b946cf495796
1hmy <33808969@qq.com>
2017-10-08 12:56:01 +0800

    直播分类

---
+++ Menu.vue
@@ -0,0 +1,92 @@
+
+<template>
+<div class="sidebar" id="sidebar">
+					
+					<div class="sidebar-shortcuts" id="sidebar-shortcuts">
+						<div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+							<button class="btn btn-success">
+								<i class="icon-signal"></i>
+							</button>
+
+							<button class="btn btn-info">
+								<i class="icon-pencil"></i>
+							</button>
+
+							<button class="btn btn-warning">
+								<i class="icon-group"></i>
+							</button>
+
+							<button class="btn btn-danger">
+								<i class="icon-cogs"></i>
+							</button>
+						</div>
+
+						<div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+							<span class="btn btn-success"></span>
+
+							<span class="btn btn-info"></span>
+
+							<span class="btn btn-warning"></span>
+
+							<span class="btn btn-danger"></span>
+						</div>
+					</div><!-- #sidebar-shortcuts -->
+
+					<ul class="nav nav-list">
+						<li class="active">
+							<a href="#">
+								<i class="icon-dashboard"></i>
+								<span class="menu-text"> 控制台 </span>
+							</a>
+						</li>
+
+						<li>
+							<a href="/#/gohome">
+								<i class="icon-text-width"></i>
+								<span class="menu-text"> 个人中心</span>
+							</a>
+						</li>
+
+						<li>
+							<a href="/#/elements" class="dropdown-toggle">
+								<i class="icon-desktop"></i>
+								<span class="menu-text"> 礼物专区 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+
+							<ul class="submenu">
+								<li>
+									<a href="/#/elements">
+										<i class="icon-double-angle-right"></i>
+										分类管理
+									</a>
+								</li>
+
+
+							</ul>
+
+						
+
+						
+							
+
+								
+
+							<ul class="submenu">
+								
+
+								
+								
+							</ul>
+						</li>
+					</ul><!-- /.nav-list -->
+
+					<div class="sidebar-collapse" id="sidebar-collapse">
+						<i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
+					</div>
+
+					
+				</div>
+
+				</template>
